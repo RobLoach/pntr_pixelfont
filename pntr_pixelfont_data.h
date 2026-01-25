38d0f1325858b872144ac1ba101d8dac83de159a
Rob Loach <robloach@gmail.com>
2026-01-24 23:22:58 -0500

    Update dependedencies

--- pntr_pixelfont_data.h
+++ pntr_pixelfont_data.h
@@ -44,11 +44,11 @@ extern const pntr_pixelfont_font PNTR_PIXELFONT_FONTS[];
 #ifndef PNTR_PIXELFONT_ENABLE_8X14
 #define PNTR_PIXELFONT_ENABLE_8X14
 #endif
-#ifndef PNTR_PIXELFONT_ENABLE_10X16
-#define PNTR_PIXELFONT_ENABLE_10X16
-#endif
 
 // TODO: Fix the following fonts.
+// #ifndef PNTR_PIXELFONT_ENABLE_10X16
+// #define PNTR_PIXELFONT_ENABLE_10X16
+// #endif
 // #ifndef PNTR_PIXELFONT_ENABLE_12X16
 // #define PNTR_PIXELFONT_ENABLE_12X16
 // #endif
